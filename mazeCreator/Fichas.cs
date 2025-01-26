814bfc64845c40d53fea1ec7e40137167043d372
SealRuinV <ernestohernandeznoa@gmail.com>
2025-01-26 17:39:27 -0500

    initial commit

---
+++ mazeCreator/Fichas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mazeCreator
+{
+    internal class Fichas
+    {
+        string name;
+        int movimiento;
+        string trampa;
+        string habilidad;
+        public Fichas(string name, int movimiento,string trampa,string habilidad) {
+            // El escurridizo
+            this.name = name;
+            this.movimiento=movimiento;
+            this.trampa=trampa;
+            this.habilidad=habilidad;
+        }
+        
+    }
+}
---
+++ mazeCreator/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MazeEscape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to maze Runner by Eduardo!");
+
+            int[,] MazeEscape= MazeGenerate(10,10);
+            // MazeCheck(ref MazeEscape);
+            ShowMaze(MazeEscape);
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine(); // Keeps the console open
+        }
+
+        public static int[,] MazeGenerate(int n, int m)
+        {
+            int[,]maze = new int[n,m];
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
+                    int seed = new Random().Next(0, 70);
+                    int cell = 0;
+                    if(seed>25&&seed<40)
+                        cell = 1;
+                    else if (seed>=40&& seed<50)
+                        cell = 2;
+                    else if (seed >= 50 && seed < 60)
+                        cell = 3;
+                    else if (seed >= 60)
+                        cell = 4;
+                    maze[i, j] = cell;
+                }
+
+            }
+            return maze;
+        }
+        public static void MazeCheck(ref int[,] maze)
+        {
+        }
+        public static void ShowMaze(int[,] maze)
+        {
+            Console.WriteLine("---------------------");
+            for (int i = 0;i < maze.GetLength(0); i++)
+            {
+                for (int j=0 ; j < maze.GetLength(1); j++)
+                {
+                    if (maze[i, j] == 0){
+                        Console.Write("| ");
+                    }
+                    else if (maze[i, j] == 1){
+                        Console.Write("|#");
+                    }
+                    else if (maze[i,j]==2)
+                    {
+                        Console.Write("|`");
+
+                    }
+                    else if (maze[i, j] == 3)
+                    {
+                        Console.Write("|-");
+
+                    }
+                    else if (maze[i, j] == 4)
+                    {
+                        Console.Write("|^");
+
+                    }
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine("---------------------");
+        }
+
+
+    }
+}
